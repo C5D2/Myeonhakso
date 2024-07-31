e3ece1fdd60ce931e10730a5bfc5d8cd7a6a7ec2
chaeyoungg <chaeyoungg77@gmail.com>
2024-07-31 18:27:13 +0900

    Design : 메인 레이아웃 css 작성

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="box-border px-10 py-3 border-b-[3px] h-[85px] flex-shrink-0">
+      <img src="/logo.svg" className="w-14 h-14" />
+    </div>
+  );
+}
