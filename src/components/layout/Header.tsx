22874246d3a95dab8fc820cfbf3b7c1c813405b6
chaeyoungg <chaeyoungg77@gmail.com>
2024-08-04 23:59:39 +0900

    Feat : 헤더 클릭 이벤트에 따른 bold 처리

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+import Categories from './Categories';
+
 export default function Header() {
   return (
     <div className="box-border px-10 py-3 border-b-[3px] h-[85px] flex-shrink-0">
-      <img src="/logo.svg" className="w-14 h-14" />
+      <div className="flex items-center gap-20">
+        <Link href="/">
+          <img src="/logo.svg" className="w-14 h-14" />
+        </Link>
+        <Categories />
+      </div>
     </div>
   );
 }
