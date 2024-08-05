7e13a3d23e05cc162152d3d920d9621be5c37929
sulahhong <100662448+sulahhong@users.noreply.github.com>
2024-08-05 20:32:17 +0900

    Chore : tailwind 커스텀 색상, 스크린 사이즈 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,9 +15,25 @@ const config: Config = {
       },
       colors: {
         'main-green': '#88B14B',
+        'main-light-green': '#E8EFDD',
+        'main-dark-green': '#81916B',
         'main-gray': '#C2C2C2',
         'main-yellow': '#FAC56C',
+        'main-red': '#D61717',
+        'light-green': '#F4FBE8',
+        'light-yellow': '#FBF9E8',
+        'light-orange': '#FFF6EF',
+        'gray-90': '#666666',  
+        'gray-30': '#D9D9D9', 
+        'gray-10': '#FAFAFA',
       },
+    },
+    screens: { // max : 00px 보다 작을 때 동작
+      sm: { max: '640px' },
+      md: { max: '768px' }, //모바일
+      lg: { max: '1024px' },
+      xl: { max: '1280px' }, //태블릿
+      xxl: { max: '1536px' },
     },
   },
   plugins: [],
