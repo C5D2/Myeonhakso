f7e717c4e2093741c7ae9fbd56b2efd491c389e8
chaeyoungg <chaeyoungg77@gmail.com>
2024-08-05 15:46:46 +0900

    Design : 스와이퍼 디자인 수정 및 svg 파일 등록

--- src/components/SwiperCard.tsx
+++ src/components/SwiperCard.tsx
@@ -11,34 +11,36 @@ import { Pagination, Navigation } from 'swiper/modules';
 import Card from './Card';
 import { useEffect, useState } from 'react';
 import { Ilecture } from '@/types/lecture';
-import { lectureFetch } from '@/data/lectureFetch';
+import { fetchLecture } from '@/data/FetchLecture';
 import useSWR from 'swr';
 
 interface SwiperCardProps {
   sortParam: string; // prop 이름과 타입 정의
 }
 const getLectures = async (path: string, sort?: object) => {
-  const data = await lectureFetch(path, sort);
+  const data = await fetchLecture(path, sort);
   return data;
 };
 
 function SwiperCard({ sortParam }: SwiperCardProps) {
   const path = 'products';
   const [sort, setSort] = useState<object>({});
+  let list;
 
   useEffect(() => {
     switch (sortParam) {
       case 'popular':
         setSort({ bookmarks: -1 });
-
         break;
+
       case 'recent':
         setSort({ createdAt: -1 });
         break;
+
       case 'teacher':
         break;
     }
-  }, []);
+  }, [sortParam]);
 
   const { data, error, isLoading } = useSWR<Ilecture[] | null>(
     [path, sort],
@@ -46,12 +48,21 @@ function SwiperCard({ sortParam }: SwiperCardProps) {
   );
 
   if (isLoading) return <p>loading...</p>;
+  if (error) return <p>error</p>;
 
-  const list = data?.map((item, index) => (
-    <SwiperSlide key={index}>
-      <Card key={index} item={item} />
-    </SwiperSlide>
-  ));
+  if (sortParam === 'popular' && data) {
+    list = data?.map((item, index) => (
+      <SwiperSlide key={index}>
+        <Card key={index} index={index} item={item} />
+      </SwiperSlide>
+    ));
+  } else {
+    list = data?.map((item, index) => (
+      <SwiperSlide key={index}>
+        <Card key={index} item={item} />
+      </SwiperSlide>
+    ));
+  }
 
   return (
     <>
@@ -61,7 +72,7 @@ function SwiperCard({ sortParam }: SwiperCardProps) {
         spaceBetween={30}
         navigation={true}
         modules={[Navigation]}
-        className="mySwiper"
+        className="cardSwiper"
       >
         {list}
         {/* <SwiperSlide>Slide 2</SwiperSlide>
@@ -71,6 +82,11 @@ function SwiperCard({ sortParam }: SwiperCardProps) {
         <SwiperSlide>Slide 6</SwiperSlide>
         <SwiperSlide>Slide 7</SwiperSlide>
         <SwiperSlide>Slide 8</SwiperSlide> */}
+
+        {/* <div className="navigation-wrapper">
+          <div className="swiper-button-prev">prev</div>
+          <div className="swiper-button-next">next</div>
+        </div> */}
       </Swiper>
     </>
   );
