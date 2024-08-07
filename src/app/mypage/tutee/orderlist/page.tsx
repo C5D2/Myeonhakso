b82087271d952a672570b98a727f6e06ae75dc95
chaeyoungg <chaeyoungg77@gmail.com>
2024-08-07 09:55:39 +0900

    Feat : 마이페이지 tutor/tutee 구분, 레이아웃 정리 등

--- src/app/mypage/tutee/orderlist/page.tsx
+++ src/app/mypage/tutee/orderlist/page.tsx
@@ -1,5 +1,14 @@
-function Page() {
-  return <div>Page</div>;
+import { fetchOrderlist } from '@/data/fetchMypage';
+
+async function Page() {
+  const data = await fetchOrderlist();
+  console.log(data);
+  return (
+    <div>
+      <h2 className="font-extrabold text-[30px]">구매 내역</h2>
+      <p>샬ㄹ라 샬라</p>
+    </div>
+  );
 }
 
 export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,23 @@ export default function RootPage() {
         <SwiperBanner />
       </div>
       <div className="max-w-[1500px] mx-auto px-10 w-full mt-10">
-        <h2 className="font-bold">지금 인기있는 강의</h2>
+        <div className="flex items-center gap-2">
+          <h2 className="font-bold text-lg">지금 인기있는 강의</h2>
+          <img src="/popular.svg" className="w-6 h-6" />
+        </div>
+
         <SwiperCard sortParam="bookmarks" />
-        <h2 className="font-bold">최근에 열린 강의</h2>
+
+        <div className="flex items-center gap-2">
+          <h2 className="font-bold">최근에 열린 강의</h2>
+          <img src="/recent.svg" className="w-6 h-6" />
+        </div>
         <SwiperCard sortParam="createdAt" />
-        <h2 className="font-bold">인기 급상승 선생님</h2>
+
+        <div className="flex items-center gap-2">
+          <h2 className="font-bold">인기 급상승 선생님</h2>
+          <img src="/teacher.svg" className="w-6 h-6" />
+        </div>
         <SwiperCard sortParam="buyQuantity" />
       </div>
     </>
