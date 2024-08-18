c70fa4a42a7ef4ada0186451e2051eb964ad9cab
chaeyoungg <chaeyoungg77@gmail.com>
2024-08-18 16:50:57 +0900

    Feat: 상품 목록 페이지 search 컴포넌트 적용

--- src/app/(edu)/[type]/Filter.tsx
+++ src/app/(edu)/[type]/Filter.tsx
@@ -1,5 +1,57 @@
 import React from 'react';
 
 export default function Filter() {
-  return <div className="border border-black my-auto w-[20%]">filterbox</div>;
+  return (
+    <div className="w-[20%]">
+      <div className="border border-gray-30 rounded-xl mx-[100px]">
+        <h3 className="p-5 font-bold">필터</h3>
+
+        <hr />
+
+        <div className="p-10 flex flex-col">
+          <select
+            name="sortName"
+            id=""
+            className="border border-gray-30 rounded-md px-5 py-1 mb-10 mx-auto w-[80%]"
+          >
+            <option value="new">최신순</option>
+            <option value="old">오래된순</option>
+            <option value="popular">인기순</option>
+            <option value="">가격높은순</option>
+            <option value="가격낮은순">가격낮은순</option>
+          </select>
+
+          <hr className="mb-10" />
+
+          <label className="font-semibold mb-3">강의 형태</label>
+          <div className="flex flex-col gap-2 mb-10">
+            <div>
+              <input type="radio" name="method" value="" /> <span>온라인</span>
+            </div>
+            <div>
+              <input type="radio" name="method" /> <span>오프라인</span>
+            </div>
+          </div>
+
+          <hr className="mb-10" />
+
+          <label className="font-semibold mb-3">난이도</label>
+          <div className="flex flex-col gap-2">
+            <div>
+              <input type="radio" /> <span>입문</span>
+            </div>
+            <div>
+              <input type="radio" /> <span>초급</span>
+            </div>
+            <div>
+              <input type="radio" /> <span>중급</span>
+            </div>
+            <div>
+              <input type="radio" /> <span>고급</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/(edu)/[type]/page.tsx
+++ src/app/(edu)/[type]/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from 'next';
 import Filter from './Filter';
 import Card from '@/components/Card';
 import Pagination from '@/components/Pagination';
+import Search from '@/components/Search';
 
 export function generateMetadata({
   params,
@@ -22,6 +23,11 @@ export function generateMetadata({
   };
 }
 
+// export const handleSearchParams = (params) => {
+//   //{{url}}/products?sort={"price":-1}&custom={"extra.level":"초급"}
+//   let filterParams = {'extra.'}
+// }
+
 async function Page({
   params,
   searchParams,
@@ -31,10 +37,10 @@ async function Page({
 }) {
   console.log(params.type);
 
-  const customParams = { extra: { type: params.type } };
+  // const customParams = { extra: { type: params.type } };
+  const customParams = { 'extra.type': params.type };
 
   const data = await fetchCategory(customParams, searchParams.page);
-  console.log('data', data);
 
   const list = data?.item?.map((item, index) => (
     <div className="w-[300px] h-[320px] rounded-xl" key={index}>
@@ -43,18 +49,23 @@ async function Page({
   ));
 
   return (
-    <div className="flex gap-x-10 gap-y-20 ">
-      <Filter />
-      <div className="flex flex-col gap-10 w-full max-w-[1320px]">
-        <div className="flex flex-wrap content-start gap-10 max-w-[1400px]">
-          {list}
-        </div>
-        <Pagination
-          page={data?.pagination?.page}
-          totalPages={data?.pagination?.totalPages}
-        />
+    <>
+      <div className="my-10">
+        <Search />
       </div>
-    </div>
+      <div className="flex gap-x-10 gap-y-20 ">
+        <Filter />
+        <div className="flex flex-col gap-10 w-full max-w-[1320px]">
+          <div className="flex flex-wrap content-start gap-10 max-w-[1400px]">
+            {list}
+          </div>
+          <Pagination
+            page={data?.pagination?.page}
+            totalPages={data?.pagination?.totalPages}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
