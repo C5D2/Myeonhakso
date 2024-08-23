d17950f028d638f4bf20d7c806f4c7af8a1255b0
C5D2 <yoakec5d2@naver.com>
2024-08-23 11:03:44 +0900

    Fix: 지도 로딩 오류 수정

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,30 +1,37 @@
 'use client';
-/* global Kakao */
+/* global kakao */
 
+import Script from 'next/script';
 import { useEffect, useRef } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 export default function KakaoMap({ address }: { address: string }) {
   const mapRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const loadKakaoMap = () => {
-      if (window.Kakao && mapRef.current) {
+      if (window.kakao && mapRef.current) {
         const container = mapRef.current;
         const options = {
-          center: new window.Kakao.maps.LatLng(33.450701, 126.570667),
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
           level: 3,
         };
-        const map = new window.Kakao.maps.Map(container, options);
+        const map = new window.kakao.maps.Map(container, options);
 
         if (address) {
-          const geocoder = new window.Kakao.maps.services.Geocoder();
+          const geocoder = new window.kakao.maps.services.Geocoder();
           geocoder.addressSearch(address, function (result: any, status: any) {
-            if (status === window.Kakao.maps.services.Status.OK) {
-              const coords = new window.Kakao.maps.LatLng(
+            if (status === window.kakao.maps.services.Status.OK) {
+              const coords = new window.kakao.maps.LatLng(
                 result[0].y,
                 result[0].x,
               );
-              const marker = new window.Kakao.maps.Marker({
+              const marker = new window.kakao.maps.Marker({
                 map: map,
                 position: coords,
               });
@@ -35,13 +42,13 @@ export default function KakaoMap({ address }: { address: string }) {
       }
     };
 
-    if (window.Kakao && window.Kakao.maps) {
+    if (window.kakao && window.kakao.maps) {
       loadKakaoMap();
     } else {
       const script = document.createElement('script');
-      script.src = `//dapi.Kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services`;
+      script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services`;
       script.onload = () => {
-        window.Kakao.maps.load(loadKakaoMap);
+        window.kakao.maps.load(loadKakaoMap);
       };
       document.head.appendChild(script);
     }
