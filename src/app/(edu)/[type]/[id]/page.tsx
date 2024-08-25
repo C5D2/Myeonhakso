588a1a3f5defe2c854762d81c46ed8cb88af5470
C5D2 <yoakec5d2@naver.com>
2024-08-25 16:05:22 +0900

    Design: 강의 상세 페이지 반응형 작업, 인기 있는 강의 카드 목록 추가

--- src/app/(edu)/[type]/[id]/page.tsx
+++ src/app/(edu)/[type]/[id]/page.tsx
@@ -4,6 +4,7 @@ import Card from '@/components/Card';
 import KakaoMap from '@/components/KakaoMap';
 import {
   fetchBookmark,
+  fetchLecture,
   fetchLectureDetail,
   fetchOtherLectures,
 } from '@/data/fetchLecture';
@@ -14,6 +15,8 @@ import BookmarkLecture from '@/components/BookmarkLecture';
 import SubscribeButton from '@/app/(edu)/[type]/[id]/SubscribeButton';
 import { calculateDays } from '@/utils/calculateDays';
 import { ShareButton } from '@/app/(edu)/[type]/[id]/ShareButton';
+import moment from 'moment';
+import LectureLevel, { ILevelType } from '@/components/LectureLevel';
 
 async function DetailPage({ params }: { params: { id: string } }) {
   const session = await getSession();
@@ -21,7 +24,12 @@ async function DetailPage({ params }: { params: { id: string } }) {
   const item = await fetchLectureDetail(params.id);
   const seller_id = item?.seller._id;
   const type = item?.extra.type;
-  const data = await fetchOtherLectures(String(seller_id!), '3');
+  const otherData = await fetchOtherLectures(String(seller_id!), '4');
+  const popularData = await fetchLecture(
+    'products',
+    { bookmarks: -1 },
+    String(4),
+  );
   // 날짜 계산
   const daysDifference = calculateDays(item?.extra?.schedule);
 
@@ -56,10 +64,10 @@ async function DetailPage({ params }: { params: { id: string } }) {
     }
   }
   console.log(subscribeId);
+  console.log(bookmarkId);
 
   // TODO: 지금 강의는 다른 강의에서 빼야됨,,,
-  // 공유하기도 하기
-  const list = data?.map((item, index) => (
+  const otherLectureList = otherData?.map((item, index) => (
     <div
       className="max-w-[300px] h-[320px] rounded-xl flex flex-grow justify-between"
       key={index}
@@ -68,17 +76,25 @@ async function DetailPage({ params }: { params: { id: string } }) {
     </div>
   ));
 
-  // TODO: 난이도마다 레벨 svg 따로~
+  const popularLectureList = popularData?.map((item, index) => (
+    <div
+      className="max-w-[300px] h-[320px] rounded-xl flex flex-grow justify-between"
+      key={index}
+    >
+      <Card key={index} item={item} />
+    </div>
+  ));
+
   return (
     <div className="relative min-h-screen">
       <div className="absolute top-0 left-0 w-full h-[50vh] bg-light-green z-0" />
 
       <div className="relative z-10 pt-20">
-        <div className="max-w-[1500px] mx-auto px-10 w-full">
+        <div className="max-w-[1500px] min-w-[380px] mx-auto px-10 w-full">
           <div className="-mt-20">
             <div className="pt-20">
-              <div className="flex gap-5 w-full">
-                <div className="rounded-3xl bg-gray-10 h-[300px] px-20 py-10 box-border flex flex-col border border-main-light-green/50 justify-evenly flex-grow">
+              <div className="flex gap-5 w-full md:flex-col">
+                <div className="rounded-3xl bg-gray-10 px-20 py-8 md:px-10 md:py-8 gap-2 box-border flex flex-col border border-main-light-green/50 justify-evenly flex-grow">
                   <p className="text-main-green font-black text-lg">
                     {item?.extra.type}
                   </p>
@@ -94,7 +110,7 @@ async function DetailPage({ params }: { params: { id: string } }) {
                     <ShareButton />
                   </div>
                 </div>
-                <div className="rounded-3xl bg-gray-10 w-[400px] max-h-[300px] p-10 box-border border border-main-light-green/50 flex flex-col gap-5">
+                <div className="rounded-3xl bg-gray-10 max-h-[300px] min-w-[450px] sm:min-w-[300px] p-10 box-border border border-main-light-green/50 flex flex-col gap-5">
                   {item?.quantity === item?.buyQuantity ? (
                     <p>이미 구매한 강의입니다.</p>
                   ) : (
@@ -130,7 +146,7 @@ async function DetailPage({ params }: { params: { id: string } }) {
                 </div>
               </div>
 
-              <div className="mt-[50px] flex gap-5">
+              <div className="mt-[50px] flex gap-5 md:flex-col">
                 <div className="flex-1 rounded-3xl bg-gray-10 px-8 py-6 border border-main-light-green/50 flex items-center justify-between">
                   <div className="flex items-center gap-10">
                     <Image
@@ -149,12 +165,7 @@ async function DetailPage({ params }: { params: { id: string } }) {
                 </div>
                 <div className="flex-1 rounded-3xl bg-gray-10 px-8 py-6 border border-main-light-green/50 flex items-center justify-between">
                   <div className="flex items-center gap-10">
-                    <Image
-                      src="/level-low.svg"
-                      width={30}
-                      height={40}
-                      alt="난이도"
-                    />
+                    <LectureLevel level={item?.extra.level as ILevelType} />
                     <div>
                       <p className="text-lg">난이도</p>
                       <p className="text-3xl font-black">{item?.extra.level}</p>
@@ -172,8 +183,13 @@ async function DetailPage({ params }: { params: { id: string } }) {
                     <div>
                       <p className="text-lg">수강 일정</p>
                       <p className="text-xl font-black">
-                        {item?.extra?.schedule[0] ?? ''} -{' '}
-                        {item?.extra?.schedule[1] ?? ''}
+                        {moment(item?.extra?.schedule[0]).format(
+                          'YYYY-MM-DD',
+                        ) ?? ''}{' '}
+                        ~{' '}
+                        {moment(item?.extra?.schedule[1]).format(
+                          'YYYY-MM-DD',
+                        ) ?? ''}
                       </p>
                     </div>
                   </div>
@@ -181,39 +197,42 @@ async function DetailPage({ params }: { params: { id: string } }) {
               </div>
 
               <h2 className="text-2xl mt-[50px] font-black">강사 소개</h2>
-              <div className="rounded-3xl bg-gray-10 h-[170px] mt-[15px] px-8 py-10 border-box  border border-main-light-green/50 flex place-content-evenly items-center gap-3">
-                {item?.seller.profileImage ? (
-                  <Image
-                    className="rounded-full"
-                    src={item?.seller.profileImage}
-                    width={80}
-                    height={80}
-                    alt="선생님 사진"
-                  />
-                ) : (
-                  <Image
-                    src="/teacher_detail.svg"
-                    width={80}
-                    height={80}
-                    alt="선생님 사진"
-                  />
-                )}
-
-                <div>
-                  <div className="flex justify-evenly items-center">
-                    <h2 className="text-2xl font-black mr-3">
-                      {item?.seller.name}
-                    </h2>
+              <div className="rounded-3xl bg-gray-10 mt-[15px] px-8 py-10 border-box border border-main-light-green/50 flex md:flex-col place-content-evenly items-center gap-3">
+                <div className="flex place-items-center gap-5">
+                  {item?.seller.profileImage ? (
                     <Image
-                      src="/star.svg"
-                      width={20}
-                      height={20}
-                      alt="만족도"
+                      className="rounded-full"
+                      src={item?.seller.profileImage}
+                      width={80}
+                      height={80}
+                      alt="선생님 사진"
                     />
-                    <p className="inline">4.8</p>
+                  ) : (
+                    <Image
+                      src="/teacher_detail.svg"
+                      width={80}
+                      height={80}
+                      alt="선생님 사진"
+                    />
+                  )}
+
+                  <div>
+                    <div className="flex justify-evenly items-center">
+                      <h2 className="text-2xl font-black mr-3">
+                        {item?.seller.name}
+                      </h2>
+                      <Image
+                        src="/star.svg"
+                        width={20}
+                        height={20}
+                        alt="만족도"
+                      />
+                      <p className="inline">4.8</p>
+                    </div>
+                    <p>선생님 소개</p>
                   </div>
-                  <p>선생님 소개</p>
                 </div>
+
                 <div className="flex flex-col gap-2">
                   <SubscribeButton
                     initialIsSubscribed={isSubscribed}
@@ -225,19 +244,22 @@ async function DetailPage({ params }: { params: { id: string } }) {
 
               <div className="mt-[50px]">
                 <h2 className="text-2xl font-black mb-4">강의 일정</h2>
-                <div className="flex gap-4">
+                <div className="flex gap-4 md:flex-col">
                   <div className="rounded-3xl w-full bg-gray-10 border border-main-light-green/50 flex flex-col gap-3 items-center justify-center py-6">
                     <h2 className="text-3xl font-black text-main-green">
                       {daysDifference}일
                     </h2>
                     <p>
-                      {item?.extra?.schedule[0] ?? ''} -{' '}
-                      {item?.extra?.schedule[1] ?? ''}
+                      {moment(item?.extra?.schedule[0]).format('YYYY-MM-DD') ??
+                        ''}{' '}
+                      ~{' '}
+                      {moment(item?.extra?.schedule[1]).format('YYYY-MM-DD') ??
+                        ''}
                     </p>
                   </div>
 
                   <div className="rounded-3xl w-full p-6 bg-gray-10 border border-main-light-green/50">
-                    <div className="flex justify-center mb-4 gap-5">
+                    <div className="flex justify-center mb-4 gap-5 md:gap-2">
                       {['월', '화', '수', '목', '금', '토', '일'].map(day => {
                         const isIncluded = item?.extra?.options.some(option =>
                           option.days.includes(day),
@@ -246,7 +268,7 @@ async function DetailPage({ params }: { params: { id: string } }) {
                           <span
                             className={`${
                               isIncluded ? 'bg-main-yellow' : 'bg-main-green'
-                            } text-white px-4 py-3 rounded-full w-12 h-12 flex items-center justify-center`}
+                            } text-white px-4 py-3 rounded-full w-12 h-12 flex items-center justify-center md:px-2`}
                             key={day}
                           >
                             {day}
@@ -269,32 +291,29 @@ async function DetailPage({ params }: { params: { id: string } }) {
                 </div>
               </div>
 
-              <div className="mt-[50px]">
-                <h2 className="text-2xl font-black">화상 강의</h2>
-                <div className="mt-5 place-content-center">
-                  <p className="text-2xl">
-                    화상강의 주소는 수강신청 완료 후 확인 가능합니다.
-                  </p>
-                  {/* <Image src="/right-arrow.svg" alt="화살표" /> */}
+              {item?.extra?.address === '' ? (
+                <div className="mt-[50px]">
+                  <h2 className="text-2xl font-black">화상 강의</h2>
+                  <div className="mt-5 place-content-center">
+                    <p className="text-2xl">
+                      화상강의 주소는 수강신청 완료 후 확인 가능합니다.
+                    </p>
+                    {/* <Image src="/right-arrow.svg" alt="화살표" /> */}
+                  </div>
                 </div>
-              </div>
-              <div className="mt-[50px]">
-                <h2 className="text-2xl font-black">대면 강의</h2>
-                <div className="rounded-3xl bg-gray-10 flex border border-main-light-green/50 mt-5 p-5">
-                  {item?.extra?.address === '' ? (
-                    <p>화상으로 진행되는 강의입니다.</p>
-                  ) : (
-                    <>
-                      <KakaoMap address={item?.extra?.address!} />
-                      <p>{item?.extra.address}</p>
-                    </>
-                  )}
+              ) : (
+                <div className="mt-[50px]">
+                  <h2 className="text-2xl font-black">대면 강의</h2>
+                  <div className="rounded-3xl bg-gray-10 border border-main-light-green/50 mt-5">
+                    <KakaoMap address={item?.extra?.address!} />
+                    <p className="p-3">{item?.extra.address}</p>
+                  </div>
                 </div>
-              </div>
+              )}
               <div className="mt-[50px]">
                 <h2 className="text-2xl font-black">강의 맛보기</h2>
                 <div
-                  className="rounded-3xl bg-gray-10 w-full relative  order border-main-light-green/50 mt-5 overflow-hidden"
+                  className="rounded-3xl bg-gray-10 w-full relative border border-main-light-green/50 mt-5 overflow-hidden"
                   style={{ paddingTop: '56.25%' }}
                 >
                   <iframe
@@ -311,12 +330,15 @@ async function DetailPage({ params }: { params: { id: string } }) {
                 <h3 className="font-bold mb-5">
                   {item?.seller.name} 선생님의 다른 강의
                 </h3>
-                <div className="flex flex-wrap content-start max-w-[1400px]">
-                  {list}
+                <div className="grid grid-cols-4 md:grid-cols-2 content-start">
+                  {otherLectureList}
                 </div>
               </div>
               <div className="mt-[50px]">
-                <h3 className="font-bold mb-5">다른 수강생이 많이 듣는 토픽</h3>
+                <h3 className="font-bold mb-5">인기 있는 토픽</h3>
+              </div>
+              <div className="grid grid-cols-4 md:grid-cols-2 content-start mb-[50px]">
+                {popularLectureList}
               </div>
             </div>
           </div>
