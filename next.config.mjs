5e99578b2998123d45eed80b7c8ebaead6be1461
C5D2 <yoakec5d2@naver.com>
2024-09-03 18:24:54 +0900

    Fix: 미들웨어를 사용하여 private routes 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      // Wildcard path matching
+      {
+        source: '/:path/:path/edit',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     domains: [
       'k.kakaocdn.net',
