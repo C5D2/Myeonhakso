c113a513ff77c8544230ad54176079aa1ba8e4ad
C5D2 <yoakec5d2@naver.com>
2024-09-03 17:59:56 +0900

    Fix: 로그인 상태가 아닐 때 결제 비활성화, 강의 상세 페이지 스타일 수정 및 강의 등록 시 알림 메시지 수정

--- src/utils/messageUtils.ts
+++ src/utils/messageUtils.ts
@@ -16,7 +16,7 @@ export function newLectureNotification(
 ) {
   return bookmarkedUsers.map(bookmarkData => ({
     target_id: bookmarkData.user_id,
-    content: `${bookmarkData.name}님, ${seller}님의 새로운 강의 ${lectureInfo.name}이/가 등록되었습니다.`,
+    content: `${bookmarkData.name}님, ${seller}님의 새로운 강의 "${lectureInfo.name}"이/가 등록되었습니다.`,
     extra: {
       lecture_id: lectureInfo.id,
       url: `${lectureInfo.type}/${lectureInfo.id}`,
